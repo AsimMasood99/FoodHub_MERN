f707012f1a54edb8f957a5f49c98b098266afe5f
AsimMasood97 <asim.masood707@gmail.com>
2024-08-10 00:38:03 +0500

    trying to deploy on render

--- FoodHub/backend/app.js
+++ FoodHub/backend/app.js
@@ -1,13 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
-
+import { config } from "dotenv";
+config();
 const app = express();
 const port = 3000;
 const databaseConnectionString =
 	"mongodb+srv://asimmasood707:plantsVsZombies@foodhub.tczkixo.mongodb.net/FoodHub";
 
-app.listen(port, () => {
-	console.log("Listning to port 3000");
+app.listen(process.env.PORT, () => {
+	console.log(`Listning to port ${process.env.port}`);
 });
 
 app.get("/", async(req,res)=>{
@@ -36,4 +37,3 @@ app.use("/api/data", dataRouter);
 import orderRouter from "./routes/order.routes.js";
 app.use("/api/orders/", orderRouter);
 
-export default app;
